cfc856367ebe61e6c232bf362ea92506d03cf3c5
Minotauro49 <34907795+Minotauro49@users.noreply.github.com>
2018-10-30 10:56:04 +1100

    Add files via upload

--- Slither-Classic/Font_Maker/TextDisplay.js
+++ Slither-Classic/Font_Maker/TextDisplay.js
@@ -1,127 +1,101 @@
+// var characterCase = "Capital";
+// var character = null;
+// var elmTag = null; 
+// var check = null;
+// var config = false;
+const element = [];
+const cssClass1 = ["btn btn-outline-primary","btn btn-outline-dark","btn btn-outline-info"];
+const cssClass2 = ["btn btn-primary","btn btn-dark","btn btn-info"];
+
 
 $(document).ready(function(){
 	$(this).contextmenu(function() {return false;}); // Removes right click menu
 	$(this).bind('dragstart',function(){return false;});
 
-	var element = [];
-	var check = false;
-	
+
+	// button toggle
 	$(this).click(function(e){
-	// 	try{ 
-	// 	switch(true){
-	// 	// Upper/lower case buttons
-		
-	// 		case e.target.firstChild.data == "UPPER - CASE": 
-	// 			if ($case!= undefined) {$case.removeClass("btn-outline-info").addClass("btn-info")}
-	// 			$case = $(e.target);
-	// 			$(e.target).removeClass("btn-info").addClass("btn-outline-info");
-	// 		break;
-
-	// 		case e.target.firstChild.data == "lower - case": 
-	// 			if ($case!=undefined) {$case.removeClass("btn-outline-info").addClass("btn-info")}
-	// 			$case = $(e.target);
-	// 			$(e.target).removeClass("btn-info").addClass("btn-outline-info");
-	// 		break;
-
-	// 		// Font size
-	// 		case e.target.firstChild.data == "Small": 
-	// 			if ($fontSize!= undefined) {$fontSize.removeClass("btn-outline-dark").addClass("btn-dark")}
-	// 			$fontSize = $(e.target);
-	// 			$(e.target).removeClass("btn-dark").addClass("btn-outline-dark");
-	// 		break;
-
-	// 		case e.target.firstChild.data == "Medium": 
-	// 			if ($fontSize!=undefined) {$fontSize.removeClass("btn-outline-dark").addClass("btn-dark")}
-	// 			$fontSize = $(e.target);
-	// 			$(e.target).removeClass("btn-dark").addClass("btn-outline-dark");
-	// 		break;
-
-	// 		case e.target.firstChild.data == "Big": 
-	// 			if ($fontSize!=undefined) {$fontSize.removeClass("btn-outline-dark").addClass("btn-dark")}
-	// 			$fontSize = $(e.target);
-	// 			$(e.target).removeClass("btn-dark").addClass("btn-outline-dark");
-	// 		break;
-	// 	}
-	// 	}catch(err) {}
-
-	const cssClass1 = ["btn btn-outline-primary","btn btn-outline-dark","btn btn-outline-secondary","btn btn-info","btn btn-dark"];
-	const cssClass2 = ["btn btn-primary","btn btn-dark","btn btn-secondary","btn btn-outline-info","btn btn-outline-dark"];
-
-	if (e.target.nodeName =="BUTTON") {
+		if (e.target.nodeName == "BUTTON" || e.target.nodeName == "LABEL" && ($(e.target).html()) != "Reset") {
 		let holdClass = e.target.className;
+console.log($.trim($(e.target).text()))
+			
+			// // toggle style
+			if ($.trim($(e.target).text()) == "UPPER - CASE") {$("#char").find("button").css("text-transform", "uppercase");characterCase = "Capital"}
+			if ($.trim($(e.target).text()) == "lower - case") {$("#char").find("button").css("text-transform", "lowercase");characterCase = "Small"}
 
-		console.log(holdClass)
-		if (element[e.target.className]!= undefined) {
-			$(element[holdClass]).removeClass(holdClass).addClass("btn btn-info");
+			// toggle prev class
+			if (!jQuery.isEmptyObject(element)) {
+				let tempElm = $(element[holdClass.split("-")[holdClass.split("-").length-1]]);
+				
+				if (typeof $(tempElm).html() != "undefined" && typeof $(e.target).html() != "undefined"){
+					if ($(tempElm).html() != $(e.target).html()) {
+						let cActive = tempElm.attr('class');
+						let cName = "btn btn-outline-primary";//cActive.substr(0,cActive.length-cActive.split(" ").pop().length-1);
+						tempElm.removeClass(cActive).addClass(cName);
+					}
+				}
+			}
+
+			element[holdClass.split("-")[holdClass.split("-").length-1]] = e.target;		
 		}
-
-		$(e.target).removeClass(holdClass).addClass(cssClass2[cssClass1.indexOf(holdClass)]);
-		element[holdClass] = e.target;
-
-
-	}
-		
 	});
-
-
-
-
-
-
-	$(".box").mousedown(function(e){
-		$(this).mousemove(function(e){ 
-			if (e.which == 1) {
-				$("ul:eq("+(e.target.parentNode.id-1)+") li:eq("+(e.target.id-1)+")").css("background-color","#27AE60");
-			}
-
-			if (e.which == 3) {
-				$("ul:eq("+(e.target.parentNode.id-1)+") li:eq("+(e.target.id-1)+")").css("background-color","");
-			}
-		})
-
-		if (e.which == 1) {
-			$("ul:eq("+(e.target.parentNode.id-1)+") li:eq("+(e.target.id-1)+")").css("background-color","#27AE60");
-		}
-
-		if (e.which == 3) {
-			$("ul:eq("+(e.target.parentNode.id-1)+") li:eq("+(e.target.id-1)+")").css("background-color","");
-		}
-	})
 });
 
 
+// // Grid controlls / data manipulation 
+// $.grid = function(){
+// 	$(".box").mousedown(function(e){ 
+// 		if (config == false) {
+// 			$(this).mousemove(function(e){
+// 				check = "Edit";
+	
+// 				if (e.which == 1) {
+// 					$("ul:eq("+(e.target.parentNode.id-1)+") li:eq("+(e.target.id-1)+")").css("background-color","#27AE60");
+// 				}
+// 				if (e.which == 3) {
+// 					$("ul:eq("+(e.target.parentNode.id-1)+") li:eq("+(e.target.id-1)+")").css("background-color","");
+// 				}
+// 			});
+// 			if (e.which == 1) {
+// 				$("ul:eq("+(e.target.parentNode.id-1)+") li:eq("+(e.target.id-1)+")").css("background-color","#27AE60");
+// 			}
+// 			if (e.which == 3) {
+// 				$("ul:eq("+(e.target.parentNode.id-1)+") li:eq("+(e.target.id-1)+")").css("background-color","");
+// 			}
+// 		}
+// 	});
+// }
+
+	
+
+// // Stores all data
+
+// var latters = [];
+// var xdata = "", ydata = "";
+// var cnt = 0;
 
 
-	// 	if (text[i] == " ") {space = 6;}else{console.log(text[i])}
-	// 	drawLatters = Alphabet("E","Small",0,0);
-	// 	for (var c = 0; c < drawLatters[0].length; c++) {
-	// 		document.getElementsByTagName('UL')[drawLatters[0][c]].getElementsByTagName('LI')[drawLatters[1][c]].style.background="teal";
-	// 	}
+// $.dataBase = function(){
+// 	$(elmTag).css("font-weight","bold");
+// 	// if (check == "Edit" ) {$(elmTag).css("font-weight","bold");check = "charPress"}
+// 	// || check == "charPress"
 
-	// var arrayLatter = [];
-	// 	arrayLatter[0] = [];
-	// 	arrayLatter[1] = [];
+// 	xdata = "";
+// 	ydata = "";
+// 	character = character.toUpperCase();
+// 	latters[character] = {"Capital":[[],[]],"Small":[[],[]]}
 
-	// document.oncontextmenu = function () {
-	//    return false;
-	// };
-	// document.addEventListener("mousedown",function(e){
-	// 	if (e.button == 0) {
-	// 		if (!(arrayLatter[0][e.target.parentNode.id+e.target.id] == e.target.parentNode.id && arrayLatter[1][e.target.parentNode.id+e.target.id] == e.target.id)) {
-	// 			document.getElementsByTagName('UL')[e.target.parentNode.id].getElementsByTagName('LI')[e.target.id].style.background="red";
-	// 			arrayLatter[0][e.target.parentNode.id+""+e.target.id] = Number(e.target.parentNode.id);
-	// 			arrayLatter[1][e.target.parentNode.id+""+e.target.id] = Number(e.target.id);
-	// 		}
-	// 	}else if (e.button == 2){
-	// 		if (arrayLatter[0][e.target.parentNode.id+e.target.id] == e.target.parentNode.id && arrayLatter[1][e.target.parentNode.id+e.target.id] == e.target.id) {
-	// 			document.getElementsByTagName('UL')[e.target.parentNode.id].getElementsByTagName('LI')[e.target.id].style.background="";
-	// 			delete arrayLatter[0][e.target.parentNode.id+""+e.target.id];
-	// 			delete arrayLatter[1][e.target.parentNode.id+""+e.target.id];
-	// 		}
-	// 	}
+// 	console.log(latters[character]["Capital"][0],latters[character]["Capital"][0][1])
+// 	for (var r = 0; r < 5; r++) {
+// 		for (var c = 0; c < 5; c++) {
 
-	// 	if (e.button == 1) {
-	// 		console.log("["+(arrayLatter[0].filter(Number).map(y => y+"+y"))+"],["+(arrayLatter[1].filter(Number).map(x => x+"+x"))+"]");
 
-	// 	}
-	// });
+// 			if ($("ul:eq("+(r)+") li:eq("+(c)+")").css("background-color") != "rgba(0, 0, 0, 0)") {
+// 				// xdata+="x+"+c+",";
+// 				// ydata+="y+"+r+",";
+// 				latters[character]["Capital"][0].push(c);
+// 				latters[character]["Capital"][1].push(r);
+// 			}
+// 		}
+// 	}
+// }
